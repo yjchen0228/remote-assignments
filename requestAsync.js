af5c477076f553be08e7cb3c9ef60bb300f00d24
yjchen0228 <yjchen0228@gmail.com>
2023-09-27 22:54:58 +0800

    w1 assignment

---
+++ requestAsync.js
@@ -0,0 +1,44 @@
+const https = require('https');
+const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
+
+function requestCallback(url, callback) {
+    const start = Date.now();
+    https.get(url, (res) => {
+        res.on('data', () => {});
+        res.on('end', () => {
+            const end = Date.now();
+            const executionTime = end - start;
+            callback(`Execution time: ${executionTime}ms`);
+        });
+    });
+}
+
+function requestPromise(url) {
+    const start = Date.now();
+    return new Promise((resolve) => {
+        https.get(url, (res) => {
+            res.on('data', () => {});
+            res.on('end', () => {
+                const end = Date.now();
+                const executionTime = end - start;
+                resolve(`Execution time: ${executionTime}ms`);
+            });
+        });
+    });
+}
+
+async function requestAsyncAwait(url) {
+    const start = Date.now();
+    await requestPromise(url);
+    const end = Date.now();
+    const executionTime = end - start;
+    console.log(`Execution time: ${executionTime}ms`);
+}
+
+const totalStart = Date.now();
+requestCallback(url, console.log);
+requestPromise(url).then(console.log);
+requestAsyncAwait(url).then(() => {
+    const totalEnd = Date.now();
+    console.log(`Total execution time: ${totalEnd - totalStart}ms`);
+});
---
+++ requestSync.js
@@ -0,0 +1,17 @@
+const request = require('sync-request');
+const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
+
+function requestSync(url) {
+    const start = Date.now();
+    const res = request('GET', url);
+    const end = Date.now();
+    const executionTime = end - start;
+    console.log(`Execution time: ${executionTime}ms`);
+}
+
+const totalStart = Date.now();
+requestSync(url);
+requestSync(url);
+requestSync(url);
+const totalEnd = Date.now();
+console.log(`Total execution time: ${totalEnd - totalStart}ms`);
